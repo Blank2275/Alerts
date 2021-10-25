07051218a9b25daa2b45dd5ad87e4c4da379c3fd
Connor <cfr3.14156@gmail.com>
2021-10-25 10:43:29 -0600

    alert with text box

--- Alerts/ViewController.swift
+++ Alerts/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var youSaidLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func alert(_ sender: Any) {
+        let alert = UIAlertController(title: "Alert", message: "You Have an Alert", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "cancel", style: .cancel, handler: nil))
+        alert.addTextField(configurationHandler: {textField in
+            textField.placeholder = "What did you say?"
+            
+        })
+        alert.addAction(UIAlertAction(title:"submit", style:.default, handler:{action in
+            if let text = alert.textFields?.first?.text{
+                self.youSaidLabel.text = "You Said: \(text)"
+            }
+        }))
+        self.present(alert, animated: true)
+    }
 }
 
